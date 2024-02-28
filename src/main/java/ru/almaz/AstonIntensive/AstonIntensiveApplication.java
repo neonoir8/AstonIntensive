2ed5a6389b0b3d716ea20e70a1530d46ede94b2a
neonoir8 <neal.neal@bk.ru>
2024-02-28 13:32:26 +0300

    first commit

---
+++ src/main/java/ru/almaz/AstonIntensive/AstonIntensiveApplication.java
@@ -0,0 +1,13 @@
+package ru.almaz.AstonIntensive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AstonIntensiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AstonIntensiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/almaz/AstonIntensive/AstonIntensiveApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.almaz.AstonIntensive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AstonIntensiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
